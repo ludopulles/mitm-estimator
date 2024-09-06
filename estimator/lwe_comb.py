53d6d3bd87225115a608b48ab99a0a412eb7b928
Ludo Pulles <ludo.pulles@gmail.com>
2024-09-06 18:59:28 +0200

    Refactor REP-0 & optimize over `r`

--- estimator/lwe_comb.py
+++ estimator/lwe_comb.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Estimate cost of solving LWE using primal attacks.
+Estimate cost of solving LWE using combinatorial attacks.
 
-See :ref:`LWE Primal Attacks` for an introduction what is available.
-
+See :ref:`LWE Combinatorial Attacks` for an introduction what is available.
+(TODO: write documentation on that page)
 """
 from sage.all import binomial, exp, floor, log, oo, RR
 
@@ -57,51 +57,60 @@ def split_weight(n):
     return (n + 1) // 2, n // 2
 
 
+def bernoulli_success(log_p, log_n):
+    """
+    Return the probability of at least 1 success when having a bernoulli distribution with
+    parameter `0 < p < 1`, and `n` many repetitions.
+    """
+    if log_p >= -20:
+        return 1.0 - exp(exp(log_n) * log(1 - exp(log_p)))
+    # For small `p`, we have 1 - (1-p)^n ~ 1 - e^{-np}.
+    x = exp(log_n + log_p)
+    if x < 1e-15:
+        # For small `x`, we have 1 - e^{-x} ~ x - x^2 / 2 + x^3 / 6 + ...
+        return x - x * x / 2.0
+    return 1.0 - exp(-x)
+
+
 class Odlyzko:
     def __call__(
         self,
         params: LWEParameters,
+        target_probability=0.99,
         log_level=1,
         **kwds
     ):
         """
-        Estimate cost of solving LWE using a MitM strategy dating back to Odlyzko.
-        Note: when comparing to [May21], they use |S|^{0.5} instead of an actual computation,
-              which should actually be the same up to polynomial factors.
+        Accurately estimate the cost of solving LWE using a MitM strategy dating back to Odlyzko.
+        Note: this means we also compute the probability that a weight-w secret splits into two
+        evenly weighted halves. This probability is >1/sqrt(n).
 
-        :param params: LWE parameters.
-        :return: A cost dictionary.
-
-        The returned cost dictionary has the following entries:
-
-        - ``rop``: Total number of word operations (≈ CPU cycles).
-        - ``mem``: The number of entries in a complete table.
+        :param params: the LWE parameters
+        :param target_probability: the desired success probability of the attack
+        :return: the cost to run this attack
         """
-
         # Check for ternary instead of sparse ternary.
         assert params.Xs.tag == "SparseTernary", "Secret distribution has to be ternary."
-        assert params.Xe.is_bounded, "Error distribution has to be bounded."
-
         # Note: Odlyzko easily extends beyond "mean = 0
         assert params.Xs.mean == 0, "Expected #1's == #-1's."
+        assert params.Xe.is_bounded, "Error distribution has to be bounded."
 
-        n = params.n
-
-        # Note: out of the `n` secret coefficients, `weight_s` are +1 and `weight_s` are -1.
-        w = params.Xs.get_hamming_weight(n) // 2
+        n, w0 = params.n, params.Xs.get_hamming_weight(params.n) // 2
 
         nl, nr = split_weight(n)
-        wl, wr = split_weight(w)
+        wl, wr = split_weight(w0)
 
+        # Odlyzko splits s into s = (s_1 || s_2), where s_1 should be of weight wl and s_2 of
+        # weight wr.
         log_S1, log_S2 = log_comb(nl, wl, wl), log_comb(nr, wr, wr)
-        log_probability = log_S1 + log_S2 - log_comb(n, w, w)
+        log_probability = log_S1 + log_S2 - log_comb(n, w0, w0)
         log_runtime = sum_log(log_S1, log_S2)
         repetitions = prob_amplify(0.99, exp(log_probability))
 
         cost = Cost(rop=exp(log_runtime), mem=exp(log_runtime))
         cost.register_impermanent(rop=True, mem=False)
         cost = cost.repeat(repetitions)
-        cost["tag"] = "Odlyzko MitM"
+        cost["tag"] = 'Odlyzko'
         return cost
 
 
@@ -109,13 +118,14 @@ class MeetRep0:
     def __call__(
         self,
         params: LWEParameters,
-        target_success_probability=0.99,
+        target_probability=0.99,
         log_level=1,
         **kwds,
     ):
         """
-        Estimate cost of solving LWE via Meet-LWE Rep-0 [May21]_.
+        Estimate cost of solving LWE using REP-0 from [May21]_.
 
+        TODO: move this to the documentation:
         The goal is to recover the ternary secret `s` from b = As + e, where the secret is of a
         special form. Namely, `s \\in T^{n}(w/2)`, where this set `T^{n}(w/2)` consists of all
         ternary vectors with the number of 1's and -1's equal to `w/2`.
@@ -131,7 +141,7 @@ class MeetRep0:
         (1) are close to a target `t` under a projection map onto `r` coefficients.
 
         :param params: LWE parameters.
-        :param target_success_probability: the desired lower bound on the probability that (a
+        :param target_probability: the desired lower bound on the probability that (a
         number of repetitions of) the algorithm yields the correct output.
         :return: A cost dictionary.
 
@@ -145,7 +155,7 @@ class MeetRep0:
             >>> from estimator import *
             >>> params = LWE.Parameters(n=200, q=127, Xs=ND.SparseTernary(200, 10), Xe=ND.UniformMod(10))
             >>> LWE.meet_rep0(params)
-            rop: ≈2^58.2, mem: ≈2^49.3, ↻: 476, tag: [May21]
+            rop: ≈2^58.2, mem: ≈2^49.3, ↻: 476, tag: REP-0
 
         """
         # Note: normalizing gives issues for the "GLP I" parameter set, so don't do that!
@@ -153,88 +163,71 @@ class MeetRep0:
 
         # Check for ternary instead of sparse ternary.
         assert params.Xs.tag == "SparseTernary", "Secret distribution has to be ternary."
-        assert params.Xe.is_bounded, "Error distribution has to be bounded."
         assert params.Xs.mean == 0, "Expected #1's == #-1's."
+        assert params.Xe.is_bounded, "Error distribution has to be bounded."
 
-        n, m, logq = params.n, params.m, RR(log(params.q))
-        if params.m == oo:
-            m = n
-
+        n, logq = params.n, RR(log(params.q))
+        n1, n2 = split_weight(n)
         error_width = params.Xe.bounds[1] - params.Xe.bounds[0] + 1
+        # The secret has `w0` coefficients equal to 1, and `w0` equal to -1.
+        w0 = params.Xs.get_hamming_weight(n) // 2
+        w1, w2 = split_weight(w0)
+        w11, w12 = split_weight(w1)
+        w21, w22 = split_weight(w2)
+
+        # number of ways to write s = s_1 + s_2
+        log_R1 = 2 * log_comb(w0, w1)
 
         logS = {}  # log(size of search space)
-        logR = {}  # log(number of representatives) (i.e. ways to write s = s1 + s2)
         logL = {}  # log(size of list of stored candidates)
 
-        # Note: out of the `n` secret coefficients, `weight_s` are +1 and `weight_s` are -1.
-        weight_s = params.Xs.get_hamming_weight(n) // 2
-
-        weight_s1, weight_s2 = split_weight(weight_s)
-
-        weight_s11, weight_s12 = split_weight(weight_s1)
-        weight_s21, weight_s22 = split_weight(weight_s2)
-        n_left, n_right = split_weight(n)
-
         # Analyse level 2.
-        logL[11] = logS[11] = log_comb(n_left, weight_s11, weight_s11)
-        logL[12] = logS[12] = log_comb(n_right, weight_s12, weight_s12)
-        logL[21] = logS[21] = log_comb(n_left, weight_s21, weight_s21)
-        logL[22] = logS[22] = log_comb(n_right, weight_s22, weight_s22)
+        logL[11] = logS[11] = log_comb(n1, w11, w11)
+        logL[12] = logS[12] = log_comb(n2, w12, w12)
+        logL[21] = logS[21] = log_comb(n1, w21, w21)
+        logL[22] = logS[22] = log_comb(n2, w22, w22)
 
         # Analyse level 1.
-        logS[1] = log_comb(n, weight_s1, weight_s1)
-        logS[2] = log_comb(n, weight_s2, weight_s2)
+        logS[1] = log_comb(n, w1, w1)
+        logS[2] = log_comb(n, w2, w2)
 
         # Number of ways that we can construct s from s1 + s2.
-        # There is flexibility where the w_s = w_s1 + w_s2 +1's come from. Similarly for -1's.
-        logR[1] = log_comb(weight_s, weight_s1) * 2.0
+        log_R1 = log_comb(w0, w1) * 2.0
 
-        # Note: `r = floor(logR[1] / logq)` is asymptotically optimal,
-        # and lower bounds the probability a representation `s = s1 + s2` survives by 1/e.
-        # However, experimentally, it can sometimes be better to increase `r` by 1 or 2.
-        # TODO: find the optimal `r` here by iteratively incrementing `r`.
-        r = floor(logR[1] / logq)
+        # Find the optimal `r` here by iteratively incrementing `r`.
+        r = floor(log_R1 / logq)
+        cur_cost = {'rop': oo}
+        while True:
+            # Warning: we make a bet on the weight distribution of:
+            #     s1 = (s11 || s12), and s2 = (s21 || s22),
+            # so we actually enumerate over LESS candidates s1 and s2!
+            logL[1] = (logS[11] + logS[12]) - r * logq
+            logL[2] = (logS[21] + logS[22]) - r * logq
 
-        # Warning: since we make a bet of s1 = (s11 || s12) and s2 = (s21 || s22),
-        # we actually enumerate over LESS candidates s1 and s2!
-        logL[1] = (logS[11] + logS[12]) - logq * r  # logS[1] - logq * r
-        logL[2] = (logS[21] + logS[22]) - logq * r  # logS[2] - logq * r
-        assert logS[11] + logS[12] <= logS[1]
-        assert logS[21] + logS[22] <= logS[2]
+            # Analyse the runtime
+            log_time_guess = RR(((r + 1) // 2) * log(error_width))
+            log_time_lists = sum_log(*logL.values())
+            log_runtime = log_time_guess + log_time_lists
 
-        # Analyse the runtime
-        log_time_error_guess = RR(((r + 1) // 2) * log(error_width))
+            # Analyse the success probability
+            prob_rep_survives = bernoulli_success(-r * logq, log_R1)
+            bet_s1 = logS[11] + logS[12] - logS[1]
+            bet_s2 = logS[21] + logS[22] - logS[2]
+            assert bet_s1 < 0 and bet_s2 < 0
 
-        # Note: (1 - 1/q^r)^R1 = (1 + 1/q^r)^{q^r * -R1/q^r} ~ e^{-R1/q^r}.
-        prob_rep_dies = exp(-exp(logR[1] - r * logq)) if r*logq > 20 else exp(exp(logR[1]) * log(1 - exp(-r * logq)))
-        # Probability that at least one of the representations survives the filter:
-        prob_rep_survives = 1.0 - prob_rep_dies
-        if prob_rep_survives == 0.0:
-            # Use approximation: 1 - e^{-1/x} ~ 1/x
-            prob_rep_survives = exp(logR[1] - r * logq)
+            log_bet = bet_s1 + bet_s2
+            repetitions = prob_amplify(target_probability, prob_rep_survives * exp(log_bet))
 
-        # Probability of matching: 0.5^{m-r},
-        # for Odlyzko-style matching on s1 and s2 respectively.
-        log_time_collisions = [logL[a] + logL[b] - (m - r) * RR(log(2))
-                               for (a, b) in [(11, 12), (21, 22)]]
-        log_time_lists = logL.values()
-        log_runtime = log_time_error_guess + sum_log(*log_time_lists, *log_time_collisions)
+            cost = Cost(rop=exp(log_runtime), mem=exp(log_runtime))
+            cost.register_impermanent(rop=True, mem=False)
+            cost = cost.repeat(repetitions)
+            cost["tag"] = 'REP-0'
 
-        # Analyse the success probability
-        bet_s1 = logS[11] + logS[12] - logS[1]  # < 0
-        bet_s2 = logS[21] + logS[22] - logS[2]  # < 0
-
-        log_probability = bet_s1 + bet_s2
-        repetitions = prob_amplify(target_success_probability,
-                                   prob_rep_survives * exp(log_probability))
-
-        cost = Cost(rop=exp(log_runtime), mem=exp(log_runtime))
-        cost.register_impermanent(rop=True, mem=False)
-        cost = cost.repeat(repetitions)
-        cost["tag"] = "[May21]"
-        return cost
-
-    __name__ = "Meet (Rep-0)"
+            if not cost <= cur_cost:
+                break
+            cur_cost = cost
+            r += 1
+        return cur_cost
 
 
 odlyzko = Odlyzko()
